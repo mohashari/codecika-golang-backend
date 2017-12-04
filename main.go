b923a6daae975bd7dcb0c59c0bd82fddc70655f6
Moh Ashari Muklis <muklis@nostratech.com>
2017-12-04 16:43:16 +0700

    first beego framework

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	_ "codecika-app/routers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func init() {
+	orm.RegisterDataBase("default", "mysql", "root:welcome1@tcp(127.0.0.1:3306)/codecika")
+}
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
